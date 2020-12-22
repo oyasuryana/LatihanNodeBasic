40820e5549a53641e81f53cfcddedbbccca1045a
oyasuryana <oyasuryana@gmail.com>
2020-12-22 22:38:20 +0700

    Modul pertama pembuatan tanggal sekarang

--- modulku.js
+++ modulku.js
@@ -1,3 +1,10 @@
+// Event     : Modul
+// Deskripsi : Membuat modul tanggal sekarang
+// Created   : 22 Desember 2020 23:38 wib 
+// author    : Oya Suryana
+// blog      : https://ozs.web.id    
+
+
 exports.tanggalKu = function() {
     return Date();
 };
