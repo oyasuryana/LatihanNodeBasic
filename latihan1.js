52b0f2bd7aacde62d3fdb6d02bff52ac80d5e7f7
oyasuryana <oyasuryana@gmail.com>
2020-12-22 22:25:39 +0700

    Latihan membuat server dengan modul http

---
+++ latihan1.js
@@ -0,0 +1,14 @@
+// Event     : Latihan 1
+// Deskripsi : Membuat server dengan modul http
+// Created   : 22 Desember 2020 23:25 wib 
+// author    :  Oya Suryana
+
+
+var http=require('http');
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end('<h4>Assalamualaikum wr.wb.</h4>');
+}).listen(9000);
+
+console.log('Server berjalan di port 9000');
